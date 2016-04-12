b7fa953131460d6e6e40c11679d94ef9f9c21e3a
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2016-04-12 10:21:28 +0200

    share place from map

--- js/app-angular.js
+++ js/app-angular.js
@@ -1,5 +1,5 @@
 // define our app and dependencies (remember to include firebase!)
-var app = angular.module("sampleApp", ["firebase"]);
+var app = angular.module("chatApp", ["firebase"]);
 
 // this factory returns a synchronized array of chat messages
 app.factory("chatMessages", ["$firebaseArray",
@@ -16,7 +16,8 @@ app.controller("ChatCtrl", ["$scope", "chatMessages",
   // we pass our new chatMessages factory into the controller
   function($scope, chatMessages) {
     $scope.user = "Chris";
-
+    $scope.currentPage = 0;
+    $scope.pageSize = 10;
     // we add chatMessages array to the scope to be used in our ng-repeat
     $scope.messages = chatMessages;
 
@@ -35,9 +36,23 @@ app.controller("ChatCtrl", ["$scope", "chatMessages",
       $scope.message = "";
     };
 
-    $scope.googleMap = function (latitude, longitude, timestamp) {
-      initMap(latitude, longitude, timestamp);
-      console.log("newmap")
+    $scope.addLocation = function(position) {
+      $scope.messages.$add({
+        author: $scope.user,
+        latitude: position.lat,
+        longitude: position.lng,
+        timestamp: Firebase.ServerValue.TIMESTAMP,
+        type: "location"
+      });
+    };
+
+    $scope.shareLocation = function() {
+      var position = mainMap.getCenter().toJSON();
+      $scope.addLocation(position);
+    };
+
+    $scope.autoScroll = function() {
+      $('.msg-panel-body').scrollTop($('.msg-panel-body')[0].scrollHeight)
     }
 
     // if the messages array are empty, add something for fun!
@@ -53,3 +68,37 @@ app.controller("ChatCtrl", ["$scope", "chatMessages",
     });
   }
 ]);
+
+app.directive('ngScrollBottom', function () {
+  return {
+    restrict: 'A',
+    scope: {
+      ngScrollBottom: "="
+    },
+    link: function (scope, element) {
+      scope.$watchCollection('ngScrollBottom', function (newValue) {
+        if (newValue)
+        {
+          $(element).scrollTop($(element)[0].scrollHeight);
+        }
+      });
+    }
+  };
+});
+
+app.directive('ngMap', function () {
+  return {
+    restrict: 'E',
+    link: function ($scope) {
+      var msg = $scope.$parent.message;
+      initMap({lat: msg.latitude, lng: msg.longitude}, msg.timestamp);
+    }
+  };
+});
+
+app.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    }
+});
--- js/maps-helloumi.js
+++ js/maps-helloumi.js
@@ -1,18 +1,54 @@
-function initMap(latitude, longitude, timestamp) {
-  var myLatLng = {lat: latitude, lng: longitude};
+var map;
+var mainMap;
 
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'+timestamp), {
-    center: myLatLng,
+function initialize () {
+  var madrid = {
+      lat: 40.4380637,
+      lng: -3.7497475
+    };
+  mainMap = new google.maps.Map(document.getElementById('map-main'), {
+    center: madrid,
     scrollwheel: false,
     zoom: 6,
     styles: [{"featureType":"all","stylers":[{"saturation":0},{"hue":"#e7ecf0"}]},{"featureType":"road","stylers":[{"saturation":-70}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"visibility":"simplified"},{"saturation":-60}]}]
   });
+  setupAutocomplete();
+}
+
+function initMap(position, timestamp) {
+  var mapId = timestamp || 'main';
+
+  // Create a map object and specify the DOM element for display.
+  map = new google.maps.Map(document.getElementById('map-{{ message.timestamp }}'), {
+    center: position,
+    scrollwheel: false,
+    zoom: 16,
+    styles: [{"featureType":"all","stylers":[{"saturation":0},{"hue":"#e7ecf0"}]},{"featureType":"road","stylers":[{"saturation":-70}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"visibility":"simplified"},{"saturation":-60}]}]
+  });
 
   // Create a marker and set its position.
+  createMarker(position, map);
+  console.log('map');
+}
+
+function createMarker(position, mapSelected) {
   var marker = new google.maps.Marker({
-    map: map,
-    position: myLatLng,
-    title: 'me'
+    position: position,
+    map: mapSelected
+  });
+}
+
+function setupAutocomplete() {
+  var input = $('#search-places')[0];
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.addListener('place_changed', function(){
+    var place = autocomplete.getPlace();
+    if (place.geometry.location) {
+      mainMap.setCenter(place.geometry.location);
+      mainMap.setZoom(17);
+      createMarker(place.geometry.location, mainMap);
+    } else {
+      alert("The place has no location...?")
+    };
   });
 }
